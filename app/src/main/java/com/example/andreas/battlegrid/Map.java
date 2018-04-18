b75591270771241068260c9c727605a560a4d7eb
Jonas <jonasgeddedah@gmail.com>
2018-04-18 17:05:03 +0200

    fix

--- app/src/main/java/com/example/andreas/battlegrid/Map.java
+++ app/src/main/java/com/example/andreas/battlegrid/Map.java
@@ -1,12 +1,7 @@
 package com.example.andreas.battlegrid;
 
 import com.example.andreas.battlegrid.Model.Action;
-<<<<<<< HEAD
-import com.example.andreas.battlegrid.Model.actions.PlayerMovment;
-import com.example.andreas.battlegrid.Model.actions.Weapon;
-=======
 import com.example.andreas.battlegrid.Model.objects.Objects;
->>>>>>> master
 import com.example.andreas.battlegrid.Model.objects.Player;
 
 import java.util.ArrayList;
@@ -48,13 +43,7 @@ public class Map {
 
     public ArrayList<ArrayList> calculatePosibleTargets(Action a){
 
-        if (a.action instanceof Weapon){ // behover ikke ta hensyn til objekter på kartet
 
-
-
-        } else { // må ta ensyn til obj på kartet, kan ikke bygge vegg oppå vegg, kan ikke gå på vegg
-
-        }
 
         return null;
     }
