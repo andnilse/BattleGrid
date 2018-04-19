26ae2a8aa2cf0329539777a4654ecf12e6e11cf8
Andreas Nilsen <andnilse@stud.ntnu.no>
2018-04-19 11:13:47 +0200

    Action classes have implemented a CalculateAllowedTarget method

--- app/src/main/java/com/example/andreas/battlegrid/Model/actions/BuildWall.java
+++ app/src/main/java/com/example/andreas/battlegrid/Model/actions/BuildWall.java
@@ -1,4 +1,14 @@
 package com.example.andreas.battlegrid.Model.actions;
 
+import com.example.andreas.battlegrid.Model.objects.Objects;
+
+import java.util.ArrayList;
+
 public class BuildWall extends Actions{
+    public boolean CalculateAllowedTargets(int x, int y, ArrayList<ArrayList<Objects>> gameMap){
+        if (gameMap.get(x).get(y) == null){
+            return true;
+        }
+        return false;
+    }
 }
--- app/src/main/java/com/example/andreas/battlegrid/Model/actions/PlayerMovment.java
+++ app/src/main/java/com/example/andreas/battlegrid/Model/actions/PlayerMovment.java
@@ -1,7 +1,16 @@
 package com.example.andreas.battlegrid.Model.actions;
 
 import com.example.andreas.battlegrid.Model.actions.Actions;
+import com.example.andreas.battlegrid.Model.objects.Objects;
+
+import java.util.ArrayList;
 
 public class PlayerMovment extends Actions {
 
+    public boolean CalculateAllowedTargets(int x, int y, ArrayList<ArrayList<Objects>> gameMap){
+        if (gameMap.get(x).get(y) == null){
+            return true;
+        }
+        return false;
+    }
 }
