98d50a8dfa40155986555aeadf7d79a740043f2a
Andreas Nilsen <andnilse@stud.ntnu.no>
2018-04-19 12:35:25 +0200

    Action classes have implemented a CalculateAllowedTarget method

--- app/src/main/java/com/example/andreas/battlegrid/Model/actions/weapons/Trap.java
+++ app/src/main/java/com/example/andreas/battlegrid/Model/actions/weapons/Trap.java
@@ -2,6 +2,9 @@ package com.example.andreas.battlegrid.Model.actions.weapons;
 
 import com.example.andreas.battlegrid.Model.actions.Weapon;
 import com.example.andreas.battlegrid.Model.objects.Objects;
+import com.example.andreas.battlegrid.Model.objects.Player;
+
+import java.util.ArrayList;
 
 public class Trap extends Objects {
     private Weapon trap;
@@ -9,5 +12,19 @@ public class Trap extends Objects {
 
     public Trap(){
 
+    }
+    public boolean CalculateAllowedTargets(int nextX, int nextY, ArrayList<ArrayList<Objects>> gameMap, Player player){
+        int x = player.getX();
+        int y = player.getY();
+        if (gameMap.get(x).get(y) == null){
+            if (x!=nextX && y!=nextY){
+                if ((nextX-1==x || (nextX+1 ==x)) && (nextY == y-1 || nextY ==y || nextY ==y+1)){
+                    if ((nextX == x) && (nextY == y+1 || nextY == y-1)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
